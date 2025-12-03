0185b19e6b09baea479a379881f3740d3e91a9f0
Milind Sharma <milindsharma8@gmail.com>
2025-12-03 23:44:53 +0800

    Day 1 part 2 completed

--- common/src/math.rs
+++ common/src/math.rs
@@ -1,4 +1,27 @@
-pub fn mod_wrap_add(pos: u32, delta: i32, mod_total: i32) -> u32 {
+
+
+pub fn mod_wrap_add(pos: u32, delta: i32, modulus: i32) -> u32 {
     // little circular buffer inspired one liner
-    ((((pos as i32 + delta) % mod_total) + mod_total) % mod_total) as u32
+    ((((pos as i32 + delta) % modulus) + modulus) % modulus) as u32
+}
+
+pub fn count_zero_crossings(current: u32, delta: i32, modulus: i32) -> u32 {
+    if delta == 0 {
+        return 0;
+    }
+    let start = current as i32;
+
+    let full_laps = (delta / modulus).unsigned_abs();
+    let remainder = delta % modulus;
+    let unwrapped_end = start + remainder;
+
+    if start != 0 {
+        // Now we need to check if the remainder causes us to cross zero.
+        // We are effectively checking to see if there was overflow in either direction.
+        let crossed_in_remainder = 
+            (unwrapped_end <= 0) ||      
+            (unwrapped_end >= modulus);
+        return full_laps + crossed_in_remainder as u32;
+    }
+    full_laps 
 }
--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,4 +1,4 @@
-use common::*;
+use common::{math::count_zero_crossings, *};
 
 const TOTAL: i32 = 100; 
 
@@ -22,16 +22,26 @@ fn part1(input: &str) -> usize {
     zeros
 }
 
-/*
-fn part2(input: &str) -> usize { todo! () }
-*/
+fn part2(input: &str) -> usize { 
+    let mut pos = 50u32;
+    let mut zero_crossings = 0;
+    let instructions = parse_lr_instructions(&input);
+    for (dir, num) in instructions {
+        let delta = match dir {
+            'L' => -(num as i32),
+            'R' => num as i32,
+            _ => 0,
+        };
+        zero_crossings += count_zero_crossings(pos, delta, TOTAL) as usize;
+        pos = mod_wrap_add(pos, delta, TOTAL);
+    }
+    zero_crossings
+}
 
 fn main() {
     let input = std::fs::read_to_string("day01/input.txt").expect("Not found - must've forgotten to put it into the root ya fool");
     println!("Part 1: {}", part1(&input));
-    /*
     println!("Part 2: {}", part2(&input));
-*/
 }
 
 #[cfg(test)]
