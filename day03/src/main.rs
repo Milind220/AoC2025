38073759e2695c87d2437d456bc0271dc239fabe
Milind Sharma <milindsharma8@gmail.com>
2025-12-06 00:05:11 +0800

    Complete day 3 part 1

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,10 +1,44 @@
-fn part1(input: &str) -> usize { todo! () }
+fn parse_batt_banks(s: &str) -> Vec<&[u8]> {
+    let mut batt_banks: Vec<&[u8]> = Vec::new();
+
+    for line in s.lines() {
+        batt_banks.push(line.as_bytes());
+    }
+    batt_banks
+}
+
+fn part1(input: &str) -> usize {
+    let battery_banks = parse_batt_banks(&input);
+    let bank_len = battery_banks[0].len(); // Since they are all the same length this is fine.
+    let mut joltage = 0u32;
+    for bank in battery_banks {
+        let max_digit = bank.iter().max().unwrap();
+        let leftmost_pos = bank.iter().position(|&b| b==*max_digit).unwrap();
+        println!("max digit {} in position {}", max_digit, leftmost_pos);
+
+        if leftmost_pos==bank.len()-1 {
+            let left_digit = bank[..bank_len-1].iter().max().unwrap();
+            println!("left digit {}", left_digit);
+            joltage += (((left_digit - b'0') * 10u8) + (max_digit - b'0')) as u32;
+        } else {
+            let right_max = bank[leftmost_pos+1..].iter().max().unwrap();
+            println!("right max {}", right_max);
+            joltage += (((max_digit - b'0') * 10u8) + (right_max - b'0')) as u32;
+        }
+    }
+
+    joltage as usize
+}
+/*
 fn part2(input: &str) -> usize { todo! () }
 
+*/
 fn main() {
-    let input = std::fs::read_to_string("input.txt").unwrap();
+    let input = std::fs::read_to_string("day03/input.txt").unwrap();
     println!("Part 1: {}", part1(&input));
+    /*
     println!("Part 2: {}", part2(&input));
+*/
 }
 
 #[cfg(test)]
