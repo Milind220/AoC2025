77a3d72f7e1e6d24a9fa577fb7b9eed3724060bc
Milind Sharma <milindsharma8@gmail.com>
2025-12-06 11:17:50 +0800

    Finish day 3 part 2

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -7,33 +7,63 @@ fn parse_batt_banks(s: &str) -> Vec<&[u8]> {
     batt_banks
 }
 
-fn get_highest_2_battery_joltage(bank: &[u8], bank_len: usize) -> u32 {
+fn get_highest_2_battery_joltage(bank: &[u8]) -> u64 {
+    let bank_len = bank.len();
     let max_digit = bank.iter().max().unwrap();
     let leftmost_pos = bank.iter().position(|&b| b==*max_digit).unwrap();
 
     if leftmost_pos==bank.len()-1 {
         let left_digit = bank[..bank_len-1].iter().max().unwrap();
-        return (((left_digit - b'0') * 10u8) + (max_digit - b'0')) as u32;
+        return (((left_digit - b'0') * 10u8) + (max_digit - b'0')) as u64;
     } else {
         let right_max = bank[leftmost_pos+1..].iter().max().unwrap();
-        return (((max_digit - b'0') * 10u8) + (right_max - b'0')) as u32;
+        return (((max_digit - b'0') * 10u8) + (right_max - b'0')) as u64;
     }
+}
+
+fn get_highest_n_battery_joltage(bank: &[u8], n: usize) -> u64{
+    let bank_len = bank.len();
+    let mut highest_n_battery_joltage = 0u64;
+    let mut pos = 0usize;
+    
+    for i in 0..n {
+        // We need to leave enough digits after this one to be able to still pick n digits total.
+        // We need (n - i - 1) digits after this one.
+        // Then max lookup index:
+        let last_possible = bank_len - (n - i);
+        let slice = &bank[pos..=last_possible];
+
+        let best_digit = slice.iter().max().unwrap();
+        let best_digit_index_in_slice = slice.iter().position(|&b| b==*best_digit).unwrap();
+        let best_digit_index = pos + best_digit_index_in_slice;
+
+        highest_n_battery_joltage = highest_n_battery_joltage * 10 + (best_digit - b'0') as u64;
+        pos = best_digit_index + 1;
+    }
+    highest_n_battery_joltage
 }
 
 fn part1(input: &str) -> usize {
     let battery_banks = parse_batt_banks(&input);
-    let bank_len = battery_banks[0].len(); // Since they are all the same length this is fine.
-    let mut joltage = 0u32;
+    let mut joltage = 0u64;
 
     for bank in battery_banks {
-        joltage += get_highest_2_battery_joltage(bank, bank_len);
+        // joltage += get_highest_2_battery_joltage(bank);
+        joltage += get_highest_n_battery_joltage(bank, 2);
     }
 
     joltage as usize
 }
 
 fn part2(input: &str) -> usize {
-    42
+    let battery_banks = parse_batt_banks(&input);
+    let mut joltage = 0u64;
+
+    for bank in battery_banks {
+        joltage += get_highest_n_battery_joltage(bank, 12);
+    }
+
+    joltage as usize
 }
 
 fn main() {
