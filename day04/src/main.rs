34d26b42145549a0bcc6687281be91002a4ec6e6
Milind Sharma <milindsharma8@gmail.com>
2025-12-09 09:06:52 +0800

    Finish day 4 part 2 using bitmap

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,24 +1,35 @@
-type Grid = Vec<Vec<u8>>;
+type Grid = Vec<Vec<u8>>; // 1 = roll exists, 0 = empty.
 
 fn parse_grid(s: &str) -> Vec<Vec<char>> {
     let mut parsed_grid = Vec::new();
     for line in s.lines() {
-        let parsed_row = line
-            .trim()
-            .chars()
-            .collect();
+        let parsed_row = line.trim().chars().collect();
         parsed_grid.push(parsed_row);
-        }
+    }
 
     parsed_grid
 }
 
-const DIRS: [(isize, isize); 8] = [
-    (-1, -1), (-1, 0), (-1, 1),
-    (0, -1),         (0, 1),
-    (1, -1), (1, 0), (1, 1)
-];
+fn to_bitmap(grid: &[Vec<char>]) -> Grid {
+    grid.iter()
+        .map(|row| {
+            row.iter()
+                .map(|&cell| if cell == '@' { 1 } else { 0 })
+                .collect()
+        })
+        .collect()
+}
 
+const DIRS: [(isize, isize); 8] = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
 
 fn count_neighbours(grid: &[Vec<char>], r: usize, c: usize) -> u8 {
     let rows = grid.len();
@@ -33,23 +44,6 @@ fn count_neighbours(grid: &[Vec<char>], r: usize, c: usize) -> u8 {
         .filter(|&cell| cell == '@')
         .count() as u8
 }
-
-fn find_accessible_rolls(grid: &[Vec<char>]) ->  Vec<(usize, usize)> {
-    let mut accessible = Vec::new();
-    let rows = grid.len();
-    let cols = grid[0].len();
-
-    for r in 0..rows {
-        for c in 0..cols {
-            if grid[r][c] == '@' && count_neighbours(grid, r, c) < 4 {
-                accessible.push((r, c));
-            }
-        }
-    }
-
-    accessible
-}
-
 
 fn part1(input: &str) -> usize {
     let grid = parse_grid(input);
@@ -67,20 +61,41 @@ fn part1(input: &str) -> usize {
 }
 
 fn count_neighbors_bit(grid: &[Vec<u8>], r: usize, c: usize) -> u8 {
-    let rows = grid.len();
-    let cols = grid[0].len();
-
     DIRS.iter()
         .filter_map(|&(dr, dc)| {
             let nr = r.checked_add_signed(dr)?;
             let nc = c.checked_add_signed(dc)?;
-            (nr < rows && nc < cols).then(|| grid[nr][nc])
+            (nr < grid.len() && nc < grid[0].len()).then(|| grid[nr][nc])
         })
         .filter(|&cell| cell == 1)
         .count() as u8
 }
 
 fn part2(input: &str) -> usize {
+    let original_grid = parse_grid(input);
+    let mut grid = to_bitmap(&original_grid);
+    let mut total = 0;
+
+    loop {
+        let mut to_remove = Vec::new();
+        for (r, row) in grid.iter().enumerate() {
+            for (c, &cell) in row.iter().enumerate() {
+                if cell == 1 && count_neighbors_bit(&grid, r, c) < 4 {
+                    total += 1;
+                    to_remove.push((r, c));
+                }
+            }
+        }
+        if to_remove.is_empty() {
+            break;
+        } else {
+            for (r, c) in to_remove {
+                grid[r][c] = 0;
+            }
+        }
+    }
+
+    total
 }
 
 fn main() {
@@ -93,8 +108,12 @@ fn main() {
 mod tests {
     use super::*;
     const INPUT: &str = include_str!("../../day04/test.txt");
-    #[test] fn test_part1() { assert_eq!(part1(INPUT), 13); }
-    /*
-    #[test] fn test_part2() { assert_eq!(part2(INPUT), 0); }
-    */
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(INPUT), 13);
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(INPUT), 42);
+    }
 }
