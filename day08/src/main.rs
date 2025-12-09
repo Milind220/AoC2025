3d7fa926537f8b6899b1a40b123457ceed56dd75
Milind Sharma <milindsharma8@gmail.com>
2025-12-09 09:07:49 +0800

    Format with cargo format

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,5 +1,9 @@
-fn part1(input: &str) -> usize { todo! () }
-fn part2(input: &str) -> usize { todo! () }
+fn part1(input: &str) -> usize {
+    todo!()
+}
+fn part2(input: &str) -> usize {
+    todo!()
+}
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
@@ -11,6 +15,12 @@ fn main() {
 mod tests {
     use super::*;
     const INPUT: &str = include_str!("../test.txt");
-    #[test] fn test_part1() { assert_eq!(part1(INPUT), 0); }
-    #[test] fn test_part2() { assert_eq!(part2(INPUT), 0); }
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(INPUT), 0);
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(INPUT), 0);
+    }
 }
