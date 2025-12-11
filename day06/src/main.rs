edd7ca978f240d003f3ff675f124785b4e217961
Milind Sharma <milindsharma8@gmail.com>
2025-12-11 10:53:33 +0800

    Finish day6 part 1

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,12 +1,57 @@
-fn part1(input: &str) -> usize {
-    todo!()
+use std::iter::zip;
+
+fn parse_input(input: &str) -> (Vec<Vec<u16>>, Vec<char>) {
+    let lines: Vec<&str> = input.lines().collect();
+    let num_rows = lines.len();
+
+    let mut rows: Vec<Vec<u16>> = Vec::new();
+    let mut operations: Vec<char> = Vec::new();
+
+    for (i, line) in lines.iter().enumerate() {
+        if i < (num_rows - 1) {
+            // Then it's a number row
+            rows.push(
+                line.split_whitespace()
+                    .filter_map(|str_num| str_num.parse::<u16>().ok())
+                    .collect(),
+            );
+        } else {
+            // then it's the operations row
+            operations = line
+                .trim()
+                .chars()
+                .filter(|&c| c == '*' || c == '+')
+                .collect();
+        }
+    }
+    (rows, operations)
 }
+
+fn part1(input: &str) -> u64 {
+    let (rows, operations) = parse_input(input);
+
+    // transpose it to have columns instead of rows
+    let columns: Vec<Vec<u64>> = (0..rows[0].len())
+        .map(|col| rows.iter().map(|row| row[col] as u64).collect())
+        .collect();
+
+    zip(columns, operations)
+        .map(|(col, op)| -> u64 {
+            match op {
+                '+' => col.iter().sum(),
+                '*' => col.iter().product(),
+                _ => panic!("WTF"),
+            }
+        })
+        .sum()
+}
+
 fn part2(input: &str) -> usize {
     todo!()
 }
 
 fn main() {
-    let input = std::fs::read_to_string("input.txt").unwrap();
+    let input = std::fs::read_to_string("day06/input.txt").unwrap();
     println!("Part 1: {}", part1(&input));
     println!("Part 2: {}", part2(&input));
 }
